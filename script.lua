0219ff09eafe6cae54bd1ef20b420dd3a1806c59
Lemon251206 <73783092+Lemon251206@users.noreply.github.com>
2023-06-06 12:21:17 +0700

    upd 3

--- script.lua
+++ script.lua
@@ -2167,7 +2167,7 @@ do
 		local frame = dropdown.List.Frame
 		
 		utility:Tween(dropdown, {Size = UDim2.new(1, 0, 0, (entries == 0 and 30) or math.clamp(entries, 0, 3) * 34 + 38)}, 0.3)
-		utility:Tween(dropdown.Search.Button, {Rotation = list and 180 or 0}, 0.3)
+		utility:Tween(dropdown.Search.Button, {Rotation = list and 0 or 0}, 0.3)
 		
 		if entries > 3 then
 		
